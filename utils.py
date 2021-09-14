79603483d0c6b2df7202f36cb4e0b41f1679d5c8
Bao Ning <robert.bao.ning@gmail.com>
2021-09-14 22:46:13 +0800

    Initialise

---
+++ utils.py
@@ -0,0 +1,9 @@
+class UltimateChineseChess:
+    '''
+        Backend function for the UltimateChineseChess
+    '''
+    def __init__(self, tutorial:bool, language:str) -> None:
+        self.__tutorial = tutorial
+        self.__language = language
+
+    def tutorial()
