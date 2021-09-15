e207800d69acd57013b8452e566fa4031b4d2174
Bao Ning <62197506+Mizuno-Mashiro@users.noreply.github.com>
2021-09-15 14:25:58 +0800

    Update settings.py

--- UltimateChineseChess_Backend/UltimateChineseChess_Backend/settings.py
+++ UltimateChineseChess_Backend/UltimateChineseChess_Backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-riyhq-8d)l%m44ml7r)ed+*293v-#9h7mw!37ln_u7!7ec&)*+'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
