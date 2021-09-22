dfdbab3d12c9abc1aa8df469a8ef6de0faf500cc
Bao Ning <robert.bao.ning@gmail.com>
2021-09-22 15:14:04 +0800

    moving folder to implement backend

--- UltimateChineseChess_Backend/UltimateChineseChess_Backend/views.py
+++ UltimateChineseChess_Backend/UltimateChineseChess_Backend/views.py
@@ -5,3 +5,9 @@ from django.http import JsonResponse
 def index(req):
     if req.method == "GET":
         return render(req, 'index.html')
+
+def startMatch(req):
+    if req.method == 'POST':
+        return JsonResponse({
+            ""
+        })
